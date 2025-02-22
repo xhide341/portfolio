65711e0f45f456b3505448ee5a3ca292d2f19514
xhide341 <shawnehgn10@gmail.com>
2025-02-23 00:08:36 +0800

    feat: integrate Astro Icon and update project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
-	integrations: [react({
-		 include: ['**/react/*', '**/magicui/*']
-		})],
+	integrations: [
+		react({
+			include: ['**/react/*', '**/magicui/*']
+		}),
+		icon(),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
